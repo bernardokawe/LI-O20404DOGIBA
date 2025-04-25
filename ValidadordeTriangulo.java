d6d8fb64a06b37191c882eb2c6108bb95455f95c
bernardokawe <bernardokawe9@gmail.com>
2025-04-25 10:31:50 -0300

    commit do kawe 2

--- ValidadordeTriangulo.java
+++ ValidadordeTriangulo.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class ValidadordeTriangulo {
     public static void main(String[] args) {
         // declaração de variáveis
-        int a, b, c; // Lados de um triangulo
+        int a, b, c; // Lados de um triângulo
 
         // Criação e instância do objeto de entrada
         Scanner entrada = new Scanner(System.in);
 
         // presentação
-        System.out.println("\n\t\t\t -- Validador de Triangulo --\n");
+        System.out.println("\n\t\t\t -- Validador de Triângulo --\n");
 
         // Entradas
         System.out.print("informe o lado A:");
@@ -22,7 +22,14 @@ public class ValidadordeTriangulo {
         // Procedimento
         if ((a < (b + c)) && (b < (a + c)) && (c < (a + b))) {
             // É triangulo
-            System.out.printf("\n%d, %d e %d forma um triangulo!\n", a, b, c);
+            System.out.printf("\n%d, %d e %d forma um triângulo!\n", a, b, c);
+            if (( a == b ) || (b == c) || (a == c)){
+            System.out.println("Triângulo isóceles");    
+        } else if ((a == b) && (b == c)){
+            System.out.println("Triângulo equilátero");
+        } else {
+            System.out.println("Triângulo escaleno");
+        }
         } else {
             // Não é triangulo
             System.out.printf("\n%d, %d e %d não foma um triangulo\n", a, b, c);
